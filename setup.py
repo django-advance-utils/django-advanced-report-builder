aeed0785c5bb3bd1f18be8c32179f5d2b4077ba5
Tom Turner <tom@twt.me.uk>
2021-11-30 10:29:08 +0000

    Included missing files

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import setuptools
-from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="django-advanced-report-builder",
-    version="0.0.3",
+    version="0.0.5",
     author="Thomas Turner",
     description="Django app that allows you to build reports from modals",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/django-advance-utils/django-advanced-report-builder",
     include_package_data=True,
-    packages=find_packages(),
+    packages=['advanced_report_builder'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
