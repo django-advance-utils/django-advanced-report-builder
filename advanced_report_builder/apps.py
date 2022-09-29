5177e79ab582e8132f2603441932eb2124ee4a02
Tom Turner <tom@twt.me.uk>
2022-09-29 13:58:05 +0100

    Update apps.py

--- advanced_report_builder/apps.py
+++ advanced_report_builder/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class ModalConfig(AppConfig):
+class AdvancedReportBuilderConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'advanced_report_builder'
     verbose_name = 'Advanced Report builder'
