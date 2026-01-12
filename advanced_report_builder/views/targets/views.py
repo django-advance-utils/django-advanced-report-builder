2340fe98e23add6e5fa3686353135b18e91a201d
Tom Turner <tom@twt.me.uk>
2026-01-12 12:25:04 +0000

    Update views.py

--- advanced_report_builder/views/targets/views.py
+++ advanced_report_builder/views/targets/views.py
@@ -9,20 +9,39 @@ from django_modals.modals import ModelFormModal
 from django_modals.processes import PERMISSION_OFF, PROCESS_EDIT_DELETE
 
 from advanced_report_builder.columns import ColourColumn
-from advanced_report_builder.form_utils import PercentageWidget
+from advanced_report_builder.form_utils import PercentageWidget, SmallInputWidget
 from advanced_report_builder.models import Target, TargetColour
 from advanced_report_builder.utils import get_query_js
 
 
 class TargetModal(ModelFormModal):
     model = Target
-    form_fields = ['name', 'target_type', 'period_type', 'default_colour', 'default_value']
+    form_fields = ['name', 'target_type', 'period_type', 'default_colour', 'default_value', 'default_percentage']
 
+    widgets = {'default_value': SmallInputWidget,
+               'default_percentage': PercentageWidget}
     process = PROCESS_EDIT_DELETE
     permission_delete = PERMISSION_OFF
 
     def form_setup(self, form, *_args, **_kwargs):
-        fields = ['name', 'target_type', 'period_type', 'default_colour', 'default_value']
+        fields = ['name', 'target_type', 'period_type', 'default_colour', 'default_value', 'default_percentage']
+
+        form.add_trigger(
+            'target_type',
+            'onchange',
+            [
+                {
+                    'selector': '#div_id_default_value',
+                    'values': {Target.TargetType.COUNT: 'show',
+                               Target.TargetType.MONEY: 'show'},
+                    'default': 'hide'},
+                {
+                    'selector': '#div_id_default_percentage',
+                    'values': {Target.TargetType.PERCENTAGE: 'show'},
+                    'default': 'hide',
+                },
+            ],
+        )
 
         if self.object.id:
             self.add_target_colours(form=form, fields=fields)
